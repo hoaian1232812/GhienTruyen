5f0b8e1c026e386963f11f42fe1ad4e56c02fe7d
Dương Thanh Phụng <20130099@st.hcmuaf.edu.vn>
2023-05-24 13:30:08 +0700

    merg

---
+++ app/src/main/java/com/app/adapter/StatisticalAdapter.java
@@ -0,0 +1,69 @@
+package com.app.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.app.R;
+import com.app.model.Topic;
+import com.app.user.TopicDetailActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatisticalAdapter extends RecyclerView.Adapter<StatisticalAdapter.StatisticalVH> {
+
+    List<String> statistics;
+    Context context;
+
+    public StatisticalAdapter() {
+        this.statistics = new ArrayList<>();
+        statistics.add("Lượt thích");
+        statistics.add("Lượt theo dõi");
+        statistics.add("Lượt đọc");
+    }
+
+    @NonNull
+    @Override
+    public StatisticalVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.item_topic, parent, false);
+        return new StatisticalVH(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StatisticalVH holder, int position) {
+        String statisticalName = statistics.get(position);
+        holder.text.setText(statisticalName);
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), TopicDetailActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("statisticalName", statisticalName);
+            intent.putExtra("data", bundle);
+            view.getContext().startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return statistics.size();
+    }
+
+
+    class StatisticalVH extends RecyclerView.ViewHolder {
+        TextView text;
+
+        public StatisticalVH(@NonNull View itemView) {
+            super(itemView);
+            text = itemView.findViewById(R.id.topic_text);
+        }
+    }
+}
---
+++ app/src/main/java/com/app/user/StatisticalFragment.java
@@ -0,0 +1,44 @@
+package com.app.user;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.R;
+import com.app.adapter.StatisticalAdapter;
+import com.app.adapter.TopicAdapter;
+import com.app.model.Topic;
+import com.app.service.ApiClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class StatisticalFragment extends Fragment {
+
+    StatisticalAdapter adapter;
+    GridLayoutManager layout;
+    RecyclerView recyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_topic, container, false);
+        setUp(root);
+        return root;
+    }
+
+    public void setUp(View root) {
+        layout = new GridLayoutManager(root.getContext(), 2);
+        recyclerView = root.findViewById(R.id.topic_list);
+        recyclerView.setLayoutManager(layout);
+
+    }
+}
